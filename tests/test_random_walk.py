a6107d4b2845d565d7b41f30e3a01bab630f03cd
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2022-01-18 20:32:56 +0100

    Added first rudimentary test

---
+++ tests/test_random_walk.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 16 19:59:07 2021
+
+@author: Alexander Southan
+"""
+
+import numpy as np
+import unittest
+
+from src.pyRandomWalk.random_walk import random_walk
+
+
+class TestRandomWalk(unittest.TestCase):
+
+    def test_random_walk(self):
+        pass
