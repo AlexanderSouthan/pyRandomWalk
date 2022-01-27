d3eeb6f4753b3852fe4b358a996cf5e8dd36a0aa
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2022-01-27 21:50:36 +0100

    Added little_helpers as requirement

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='pyRandomWalk',
     version='0.0.1',
     packages=find_packages(where='src'),
-    install_requires=['numpy', 'pandas']
+    install_requires=['numpy', 'pandas', 'little_helpers']
 )
