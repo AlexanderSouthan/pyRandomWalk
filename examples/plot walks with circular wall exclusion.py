e618b95bf9e90363e6828bd5d4a9cc7468b24475
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2022-02-24 00:17:18 +0100

    Renamed examples and added example with circular box

--- examples/plot walks with circular wall exclusion.py
+++ examples/plot walks with circular wall exclusion.py
@@ -12,18 +12,16 @@ import matplotlib.patches as patches
 from pyRandomWalk import random_walk
 
 # Define the box for the random walks
-limits = {'x': [-1, 1], 'y': [-1, 1]}
+limits = {'r': 0.75, 'x_c': 0.5, 'y_c': 0.5}
 
 # Generate random walks
 random_walks = random_walk(
-    step_number=20, number_of_walks=2, limits=limits, wall_mode='reflect',
-    step_length=0.5)
-
+    step_number=200, number_of_walks=2, limits=limits, wall_mode='exclude',
+    step_length=0.5, box_shape='circle')
 
 # Query the different coordinates that are used for the plots
 all_coords = random_walks.get_coords('all')
 walk_coords = random_walks.get_coords('walk_points')
-reflect_coords = random_walks.get_coords('reflect_points')
 end_coords = random_walks.get_coords('end_points')
 
 
@@ -34,19 +32,15 @@ for curr_walk in range(random_walks.number_of_walks):
              ls='none', marker='o', ms='10', c='g')
     ax1.plot(walk_coords[curr_walk][:, 0], walk_coords[curr_walk][:, 1],
              ls='none', marker='o', c=['k', 'grey'][curr_walk % 2])
-    ax1.plot(reflect_coords[curr_walk][:, 0], reflect_coords[curr_walk][:, 1],
-             ls='none', marker='o', c='r')
     ax1.plot(all_coords[curr_walk][:, 0], all_coords[curr_walk][:, 1],
-             c=['k', 'grey'][curr_walk % 2])
+              c=['k', 'grey'][curr_walk % 2])
 
-ax1.set_xlim([-1.1, 1.1])
-ax1.set_ylim([-1.1, 1.1])
-box = patches.Rectangle((random_walks.limits[0, 0], random_walks.limits[1, 0]),
-                        random_walks.limits[0, 1]-random_walks.limits[0, 0],
-                        random_walks.limits[1, 1]-random_walks.limits[1, 0],
-                        linewidth=1, edgecolor='k', facecolor='none', ls='--')
+ax1.set_xlim([-0.6, 1.6])
+ax1.set_ylim([-0.6, 1.6])
+box = patches.Circle((limits['x_c'], limits['y_c']), radius=limits['r'],
+                     linewidth=1, edgecolor='k', facecolor='none', ls='--')
 ax1.add_patch(box)
 ax1.set_aspect('equal', adjustable='box')
 fig1.set_facecolor('grey')
 
-fig1.savefig('plot walks with wall reflections.png', dpi=600)
+fig1.savefig('plot walks with circular wall exclusion.png', dpi=600)
---
+++ examples/plot walks with rectangular wall exclusion.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 19 19:05:05 2022
+
+@author: Alexander Southan
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+from pyRandomWalk import random_walk
+
+# Define the box for the random walks
+limits = {'x': [-1, 1], 'y': [-1, 1]}
+
+# Generate random walks
+random_walks = random_walk(
+    step_number=20, number_of_walks=2, limits=limits, wall_mode='exclude',
+    step_length=0.5)
+
+# Query the different coordinates that are used for the plots
+all_coords = random_walks.get_coords('all')
+walk_coords = random_walks.get_coords('walk_points')
+end_coords = random_walks.get_coords('end_points')
+
+
+# Plot the random walks
+fig1, ax1 = plt.subplots()
+for curr_walk in range(random_walks.number_of_walks):
+    ax1.plot(end_coords[curr_walk][:, 0], end_coords[curr_walk][:, 1],
+             ls='none', marker='o', ms='10', c='g')
+    ax1.plot(walk_coords[curr_walk][:, 0], walk_coords[curr_walk][:, 1],
+             ls='none', marker='o', c=['k', 'grey'][curr_walk % 2])
+    ax1.plot(all_coords[curr_walk][:, 0], all_coords[curr_walk][:, 1],
+              c=['k', 'grey'][curr_walk % 2])
+
+ax1.set_xlim([-1.1, 1.1])
+ax1.set_ylim([-1.1, 1.1])
+box = patches.Rectangle((random_walks.limits[0, 0], random_walks.limits[1, 0]),
+                        random_walks.limits[0, 1]-random_walks.limits[0, 0],
+                        random_walks.limits[1, 1]-random_walks.limits[1, 0],
+                        linewidth=1, edgecolor='k', facecolor='none', ls='--')
+ax1.add_patch(box)
+ax1.set_aspect('equal', adjustable='box')
+fig1.set_facecolor('grey')
+
+fig1.savefig('plot walks with rectangular wall exclusion.png', dpi=600)
